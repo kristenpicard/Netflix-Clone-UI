dee37be205c878c0488169ba5af28478cbcbbcf7
Kristen Picard <kristenpicard93@gmail.com>
2021-08-06 14:11:18 -0500

    Initial commit with some Nav work

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./normalize.css"
+import Navigation from "./components/Navigation";
+
+function App() {
+  return <div className="App">
+    <Navigation />
+    {/* <Billboard />
+    <Titles />
+    <Footer /> */}
+  </div>;
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Navigation.module.css'
+
+const Navigation = () => {
+    return (
+        <div className={styles.navigation}>
+            Navigation
+        </div>
+    )
+}
+
+export default Navigation;
