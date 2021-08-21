7641571aaf72e7ae7cbc3a3f844dab9f8307a8fe
Kristen Picard <kristenpicard93@gmail.com>
2021-08-21 14:45:27 -0500

    adding description to billboard

--- src/components/Billboard/Billboard.jsx
+++ src/components/Billboard/Billboard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import billboardHeroTitle from './assets/billboard-title.webp';
 import billboardHeroImg from './assets/billboard.webp';
 import styles from './Billboard.module.css';
+import PlayIcon from './PlayIcon';
+import InfoIcon from './InfoIcon';
 
 const Billboard = () => {
     return (
@@ -10,6 +12,20 @@ const Billboard = () => {
                 <img src={billboardHeroImg} alt="" />
                 <div className={styles.info}>
                     <img src={billboardHeroTitle} alt="Abstract: The Art of Design" />
+                    <div className={styles.description}>
+                        Step inside the minds of the most innovative designers in a variety of disciplines and learn how
+                        design impacts every aspect of life.
+                    </div>
+                    <div className={styles.links}>
+                    <a href="/">
+                        <PlayIcon />
+                        <span>Play</span>
+                    </a>
+                    <button>
+                        <InfoIcon />
+                        <span>More Info</span>
+                    </button>
+                    </div>
                 </div>
             </div>
         </div>
