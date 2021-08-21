6b4e673e8af184d25c09ce29a3a58b76853c5236
Kristen Picard <kristenpicard93@gmail.com>
2021-08-21 14:19:39 -0500

    Fixing billboard

--- src/components/Billboard/Billboard.jsx
+++ src/components/Billboard/Billboard.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import billboardHeroTitle from './assets/billboard-title.webp';
+import billboardHeroImg from './assets/billboard.webp';
 import styles from './Billboard.module.css';
 
 const Billboard = () => {
     return (
         <div className={styles.billboard}>
             <div className={styles.innerBillboard}>
-sdfsdfs
+                <img src={billboardHeroImg} alt="" />
+                <div className={styles.info}>
+                    <img src={billboardHeroTitle} alt="Abstract: The Art of Design" />
+                </div>
             </div>
         </div>
     )
